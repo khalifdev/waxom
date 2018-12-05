fd721bf58bd9fc28708e999fb4e8895f4e0364b7
khalifdev <n.khalif@yandex.ru>
2018-12-05 23:25:59 +0300

    Футер осталось доделать. Не получился слайдер с использованием сторонней библиотеки в секции Recent Posts

---
+++ script.js
@@ -0,0 +1,72 @@
+/** @var {object} media - объект видеозаписи*/
+var media;
+/** @var {object} play - кнопка play/pause */
+var play;
+/** контейнер таймера под видео */
+var size_box;
+/** кнопки переключения картинок в Recent Posts */
+var left_btn;
+var right_btn;
+/** номер картинки(поста) для переключения */
+var n = 1;
+/**
+ * Функция инициализирует все элементы проигрывателя и вешает
+ * на его элементы управления обработчики событий.
+ */
+function initiate() {
+    //получаем объект видеозаписи
+    media = document.getElementById('media');
+    //получаем объект кнопки play
+    play = document.getElementById('play_button');
+    size_box = document.getElementById('size');
+    //вешаем обработчик события click на кнопку play/pause
+    play.addEventListener('click', playPauseClick);
+    left_btn = document.getElementById('bracket_left');
+    right_btn = document.getElementById('bracket_right');
+}
+/**
+ * Функция показыает на шкале времени сколько видео уже просмотрено
+ */
+function status() {
+    //если просмотр не закончен
+    if (!media.ended) {
+        //получаем сколько уже просмотрено в процентах
+        var size = media.currentTime.toTimeString();
+        size_box.innerHTML = size;
+    }
+}
+/**
+ * Функция вызывается при нажатии на кнопку Play/Pause
+ */
+function playPauseClick() {
+    //если видеозапись не стоит на паузе и не дошло до конца
+    if (!media.paused && !media.ended) {
+        //ставим видео на паузу
+        media.pause();
+    }
+    else {
+        //запускаем видеозапись
+        media.play();
+        setInterval(status, 1000);
+    }
+}
+$(document).ready(function () {
+    initiate();
+    right_btn.onclick = (function () {
+        $('.post' + n).removeClass('hov hov'+n);
+        if (n == 3) n = 1;
+        else n++;
+        $('.post' + n).addClass('hov hov'+n);
+    });
+    left_btn.onclick = (function () {
+        $('.post' + n).removeClass('hov hov'+n);
+        if (n == 1) n = 3;
+        else n--;
+        $('.post' + n).addClass('hov hov'+n);
+    });
+    $('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+});
+});
